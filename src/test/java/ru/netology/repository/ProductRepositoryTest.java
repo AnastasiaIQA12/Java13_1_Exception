388851c0ac968bb650e26b12aa17ba30e031ceb6
AnastasiaIQA12 <Sinka8900@mail.ru>
2020-12-11 16:37:22 +0400

    Add ProductManager and NotFoundException

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -7,15 +7,38 @@ import ru.netology.domain.Product;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
-  private ProductRepository repository = new ProductRepository();
-  private Book coreJava = new Book();
-
   @Test
-  public void shouldSaveOneItem() {
-    repository.save(coreJava);
-
-    Product[] expected = new Product[]{coreJava};
-    Product[] actual = repository.findAll();
+  void shouldRemoveByIdYes() {
+    ProductRepository repository=new ProductRepository();
+    Book book1=new Book(1,"Cinderella",103,"SharlPerro",300,2015);
+    Book book2=new Book(2,"Pinocchio",170,"Tolstoi",105,2019);
+    Book book3=new Book(3,"Mermaid",407,"Anderson",456,2017);
+    Book book4=new Book(4,"iPhone 12",354,"NoAuthor",234, 2016);
+    repository.save(book1);
+    repository.save(book2);
+    repository.save(book3);
+    repository.save(book4);
+    repository.removeById(3);
+    Product[] actual=repository.findAll();
+    Product[] expected = new Product[]{book1,book2,book4};
     assertArrayEquals(expected, actual);
   }
+
+  @Test
+  void shouldRemoveByIdNo() {
+    ProductRepository repository=new ProductRepository();
+    Book book1=new Book(1,"Cinderella",103,"SharlPerro",300,2015);
+    Book book2=new Book(2,"Pinocchio",170,"Tolstoi",105,2019);
+    Book book3=new Book(3,"Mermaid",407,"Anderson",456,2017);
+    Book book4=new Book(4,"iPhone 12",354,"NoAuthor",234, 2016);
+    repository.save(book1);
+    repository.save(book2);
+    repository.save(book3);
+    repository.save(book4);
+    repository.removeById(5);
+    Product[] actual=repository.findAll();
+    Product[] expected = new Product[]{book1,book2,book3,book4};
+    assertArrayEquals(expected, actual);
+  }
+
 }
